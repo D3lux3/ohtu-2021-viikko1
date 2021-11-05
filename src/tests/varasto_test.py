b50e3434d473a27d96a8efc9bd2fd816d888460a
D3lux3 <38981163+D3lux3@users.noreply.github.com>
2021-11-05 19:43:26 +0200

    Testit tehty

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -6,6 +6,33 @@ class TestVarasto(unittest.TestCase):
     def setUp(self):
         self.varasto = Varasto(10)
 
+    def test_negatiivinen_tilavuus(self):
+        self.assertEquals(Varasto(-1).tilavuus, 0)
+
+    def test_negatiivinen_alkusaldo(self):
+        self.assertEquals(Varasto(0, -1).saldo, 0)
+    
+    def test_negatiivinen_lisays(self):
+        self.assertEquals(self.varasto.lisaa_varastoon(-1), None)
+
+    def test_valmiiksi_taynna(self):
+        self.assertEquals(Varasto(5, 10).saldo, 5)
+    
+    def test_liiallisen_lisays(self):
+        self.varasto.lisaa_varastoon(500)
+        self.assertEquals(self.varasto.saldo, self.varasto.tilavuus)
+
+    def test_tulostus_oikea(self):
+        self.assertEquals(str(self.varasto), "saldo = 0, vielä tilaa 10")
+
+    def test_liiallinen_ottaminen_varastosta(self):
+        self.varasto.lisaa_varastoon(1)
+        self.assertEquals(self.varasto.ota_varastosta(20), 1)
+        self.assertEquals(self.varasto.saldo, 0.0)
+
+    def test_ota_negatiivinen_varastosta(self):
+        self.assertEquals(self.varasto.ota_varastosta(-50), 0.0)
+
     def test_konstruktori_luo_tyhjan_varaston(self):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
